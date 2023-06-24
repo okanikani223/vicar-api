64f30f9ec3b1390dbb500bc7abee8a6f146ddfe3
okanikani223 <k.hidaka63@gmail.com>
2023-06-24 12:02:55 +0900

    dev commit

---
+++ src/main/java/com/github/okanikani223/vicar/download/others/services/DownloadRequestReceptionServiceImpl.java
@@ -0,0 +1,33 @@
+package com.github.okanikani223.vicar.download.others.services;
+
+import com.github.okanikani223.vicar.download.domain.model.repositories.DownloadItemRepository;
+import com.github.okanikani223.vicar.download.domain.model.vo.DownloadRequest;
+import com.github.okanikani223.vicar.download.domain.services.DownloadRequestReceptionService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutorService;
+
+@Component
+@RequiredArgsConstructor
+public class DownloadRequestReceptionServiceImpl implements DownloadRequestReceptionService {
+    private final ExecutorService executorService;
+    private final Logger logger;
+//    private final DownloadItemRepository downloadItemRepository;
+
+    @Override
+    public void receive(DownloadRequest downloadRequest) throws Exception {
+        logger.debug(downloadRequest.toString());
+//        downloadItemRepository.save(downloadRequest.toItem());
+        executorService.submit(createDownloader(downloadRequest));
+    }
+
+    private Runnable createDownloader(DownloadRequest downloadRequest) {
+        var url = downloadRequest.requestUrl();
+
+        return () -> {
+            logger.info(url.toString());
+        };
+    }
+}
